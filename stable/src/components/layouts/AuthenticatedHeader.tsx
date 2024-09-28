fe5305f56ee7ec1f121b9e7a4c0d859876685a8e
Darce <88146915+Darce256@users.noreply.github.com>
2024-09-28 07:56:13 -0400

    Updated Layout

--- stable/src/components/layouts/AuthenticatedHeader.tsx
+++ stable/src/components/layouts/AuthenticatedHeader.tsx
@@ -1,4 +1,5 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import {
   Breadcrumb,
@@ -17,10 +18,22 @@ import {
   DropdownMenuSeparator,
   DropdownMenuItem,
 } from "@/components/ui/dropdown-menu";
+import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet";
 import { useAuth } from "@/components/Contexts/AuthProvider";
+import {
+  MenuIcon,
+  StoreIcon,
+  UsersIcon,
+  InfoIcon,
+  SettingsIcon,
+} from "lucide-react";
+import { FaHorseHead } from "react-icons/fa6";
+import { TbHorseshoe } from "react-icons/tb";
 
 export default function AuthenticatedHeader() {
   const { user, logout } = useAuth();
+  const [isOpen, setIsOpen] = useState(false);
+  const location = useLocation();
 
   const getInitials = (name: string) => {
     const names = name.split(" ");
@@ -28,29 +41,71 @@ export default function AuthenticatedHeader() {
     return initials.toUpperCase();
   };
 
+  const navItems = [
+    { name: "Dashboard", icon: StoreIcon, href: "/dashboard" },
+    { name: "Horses", icon: FaHorseHead, href: "/horses" },
+    { name: "New Shoeings", icon: TbHorseshoe, href: "/new-shoeings" },
+    { name: "Customers", icon: UsersIcon, href: "/customers" },
+    { name: "Analytics", icon: InfoIcon, href: "/analytics" },
+    { name: "Settings", icon: SettingsIcon, href: "/settings" },
+  ];
+
+  const getCurrentPageName = () => {
+    const currentPath = location.pathname;
+    const currentItem = navItems.find((item) => item.href === currentPath);
+    return currentItem ? currentItem.name : "Overview";
+  };
+
   return (
-    <header className="sticky top-0 z-30 flex h-16 items-center gap-4 border-b bg-background px-4 sm:px-6 pt-0">
+    <header className="sticky top-0 z-30 flex h-16 sm:h-16 items-center gap-4 border-b bg-background px-4 sm:px-6 pt-0 sm:pt-0">
+      {/* Mobile Menu */}
+      <Sheet open={isOpen} onOpenChange={setIsOpen}>
+        <SheetTrigger asChild>
+          <Button
+            size="icon"
+            variant="outline"
+            className="mr-2 sm:hidden"
+            onClick={() => setIsOpen(true)}
+          >
+            <MenuIcon className="h-5 w-5" />
+            <span className="sr-only">Toggle Menu</span>
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left" className="w-[250px] sm:max-w-sm">
+          <nav className="grid gap-6 text-lg font-medium">
+            {navItems.map((item) => (
+              <Link
+                key={item.name}
+                to={item.href}
+                className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+                onClick={() => setIsOpen(false)}
+              >
+                <item.icon className="h-5 w-5" />
+                {item.name}
+              </Link>
+            ))}
+          </nav>
+        </SheetContent>
+      </Sheet>
+
       <Breadcrumb className="hidden md:flex">
         <BreadcrumbList>
           <BreadcrumbItem>
             <BreadcrumbLink asChild>
-              <Link to="#">Dashboard</Link>
+              <Link to="/dashboard">Dashboard</Link>
             </BreadcrumbLink>
           </BreadcrumbItem>
-          <BreadcrumbSeparator />
-          <BreadcrumbItem>
-            <BreadcrumbPage>Overview</BreadcrumbPage>
-          </BreadcrumbItem>
+          {location.pathname !== "/dashboard" && (
+            <>
+              <BreadcrumbSeparator />
+              <BreadcrumbItem>
+                <BreadcrumbPage>{getCurrentPageName()}</BreadcrumbPage>
+              </BreadcrumbItem>
+            </>
+          )}
         </BreadcrumbList>
       </Breadcrumb>
-      <div className="relative ml-auto flex-1 md:grow-0">
-        <div className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-        <Input
-          type="search"
-          placeholder="Search..."
-          className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
-        />
-      </div>
+      <div className="relative ml-auto flex-1 md:grow-0"></div>
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button
--- stable/src/components/layouts/AuthenticatedLayout.tsx
+++ stable/src/components/layouts/AuthenticatedLayout.tsx
@@ -1,18 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "@/components/layouts/Sidebar";
 import AuthenticatedHeader from "./AuthenticatedHeader";
+import { Footer } from "../ui/nav/Footer";
 
 export default function AuthenticatedLayout() {
   return (
     <div className="flex min-h-screen">
       <Sidebar />
-      <div className="flex-1 flex flex-col ml-14">
-        {" "}
-        {/* Add ml-14 here */}
+      <div className="flex-1 flex flex-col sm:ml-14">
         <AuthenticatedHeader />
-        <main className="flex-1 p-4 sm:p-6 md:p-8">
+        <main className="flex-1 p-4 sm:p-6 md:p-8 mt-16 sm:mt-0">
           <Outlet />
         </main>
+        <Footer />
       </div>
     </div>
   );
