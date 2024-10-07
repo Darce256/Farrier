5acbd31e60b9892131a5e1def328ba396d7b9056
Darce <88146915+Darce256@users.noreply.github.com>
2024-10-07 09:49:00 -0400

    Better calendar popup

--- stable/src/components/pages/Calendar.tsx
+++ stable/src/components/pages/Calendar.tsx
@@ -684,20 +684,34 @@ export default function Calendar() {
         open={isDuplicateDialogOpen}
         onOpenChange={setIsDuplicateDialogOpen}
       >
-        <DialogContent>
-          <DialogHeader>
+        <DialogContent className="sm:max-w-[425px] w-full p-2 ">
+          <DialogHeader className="p-4 pb-2">
             <DialogTitle>Duplicate Shoeings</DialogTitle>
           </DialogHeader>
-          <div className="py-4">
+          <div className="px-4 pb-4">
             <CalendarPicker
               mode="single"
               selected={duplicateDate}
               onSelect={setDuplicateDate}
               disabled={(date) => date <= new Date()}
               initialFocus
+              className="h-full w-full flex"
+              classNames={{
+                months:
+                  "flex w-full flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0 flex-1",
+                month: "space-y-4 w-full flex flex-col",
+                table: "w-full h-full border-collapse space-y-1",
+                head_row: "",
+                row: "w-full mt-2",
+              }}
+              style={{ width: "100%" }}
             />
           </div>
-          <Button onClick={handleDuplicateShoeings} disabled={!duplicateDate}>
+          <Button
+            onClick={handleDuplicateShoeings}
+            disabled={!duplicateDate}
+            className="w-full rounded-none rounded-b-lg"
+          >
             Duplicate
           </Button>
         </DialogContent>
