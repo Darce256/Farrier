bfbc9fae48b6fe4ccc5ea5349aca105d9facb37e
Darce <88146915+Darce256@users.noreply.github.com>
2024-10-10 20:43:47 -0400

    admin

--- stable/src/components/PermissionsTab.tsx
+++ stable/src/components/PermissionsTab.tsx
@@ -117,7 +117,7 @@ export function PermissionsTab() {
           placeholder="Search users..."
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
-          className="pl-10"
+          className="pl-10 bg-white"
         />
         <Search
           className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400"
