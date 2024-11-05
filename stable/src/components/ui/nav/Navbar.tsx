bdb4ba24ad29735ca9b81f22309a057c6c413de5
Darcy <aiurish@gmail.com>
2024-11-05 09:43:35 -0500

    just homepage update

--- stable/src/components/ui/nav/Navbar.tsx
+++ stable/src/components/ui/nav/Navbar.tsx
@@ -28,10 +28,7 @@ export default function NavBar() {
 
   const toggleMenu = () => setIsOpen(!isOpen);
 
-  const navItems = [
-    { name: "Home", href: "/" },
-    { name: "Contact", href: "/contact" },
-  ];
+  const navItems = [{ name: "Home", href: "/" }];
 
   return (
     <nav className="bg-primary">
